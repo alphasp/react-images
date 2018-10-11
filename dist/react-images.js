b5d3d46485d708082027d01ba753bce7af0f7a91
alphasp <gmerudotcom@gmail.com>
2018-10-11 21:22:02 +0800

    build

--- dist/react-images.js
+++ dist/react-images.js
@@ -1161,6 +1161,8 @@ var Lightbox = function (_Component) {
 			// make sure event only happens if they click the backdrop
 			// and if the caption is widening the figure element let that respond too
 			if (event.target.id === 'lightboxBackdrop' || event.target.tagName === 'FIGURE') {
+				event.preventDefault();
+				event.stopPropagation();
 				this.props.onClose();
 			}
 		}
